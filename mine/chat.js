a860067b0619dee48aac454d3c723ceda845a003
keck <github@perlhack.com>
2015-02-19 17:21:06 -0500

    checkpoint: added username+socket pairing

--- mine/chat.js
+++ mine/chat.js
@@ -2,16 +2,37 @@ var app = require('express.io')();
 app.http().io();
 var port = process.env.PORT || 3000;
 
-app.io.route('chat message', function(req) {
-    console.log("got message!");
-    req.io.emit('chat message', {
+var clients = {};
+var clientSockets = {};
+
+// routes
+app.io.route('speak', function(req) {
+    console.log("-> "+req.data)
+    req.io.broadcast('speak', {
+        user: clientSockets[req.socket.id],
         message: req.data
-    })
-})
+    });
+});
+
+app.io.route('set username', function(req) {
+    var username = req.data;
+    console.log("attempting to set username -> "+req.data+":"+req.socket.id);
+    if(clients[username] === undefined) {
+        clients[username] = req.socket.id;
+        clientSockets[req.socket.id] = username;
+        console.log("paired username:websocket -> "+req.data+":"+req.socket.id);
+        req.io.respond({ OK: 'username set successfully' });
+    } else {
+        req.io.respond({ ERROR: 'username in use' });
+    }
+
+});
+
+
 
 // Client HTML
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/public/index.html');
-})
+});
 
 app.listen(port);
