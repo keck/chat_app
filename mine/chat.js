1b31df3bd048eea012d6a0892aeaad8f65f9c5d6
keck <github@perlhack.com>
2015-02-19 22:55:08 -0500

    checkpoint: whoami works

--- mine/chat.js
+++ mine/chat.js
@@ -6,7 +6,7 @@ var clients = {};
 var clientSockets = {};
 
 // List of valid commands
-var cmds = ["join", "part", "list", "channels", "nick", "msg"];
+var cmds = ["join", "part", "list", "channels", "nick", "whoami", "msg"];
 
 // Build a string of cmds usable in a regex
 var cmdString = "";
@@ -15,10 +15,10 @@ cmds.forEach( function(e, i, a){
 });
 cmdString = cmdString.slice(0, cmdString.length - 1);
 
-//var cmdRe = new RegExp('^\s*\/('+cmdString+')\s?', 'i');
-var cmdRe = new RegExp('^\ *\/('+cmdString+')(.*)$', 'i');
+var cmdRE = new RegExp('^\ *\/('+cmdString+')(.*)$', 'i');
+var userRE = new RegExp('/^[a-zA-Z][a-zA-Z0-9]+$/');
 
-console.log("Valid commands: " + cmdRe);
+console.log("Valid commands: " + cmdRE);
 
 // routes
 app.io.route("speak", function(req) {
@@ -27,13 +27,13 @@ app.io.route("speak", function(req) {
     console.log("-> "+data);
     // Commands we want to look for:
     // join, part, list, channels, nick, msg
-    if( cmdRe.test(data) ) {
-        var cmd = data.match(cmdRe)[1];
-        var args = data.match(cmdRe)[2].trim();
-        console.log("got cmd + args " + cmd + args);
+    if( cmdRE.test(data) ) {
+        var cmd = data.match(cmdRE)[1];
+        var args = data.match(cmdRE)[2].trim();
 
         var fname = "cmd_" + cmd.toLowerCase();
-        eval(fname)(args);
+        // I'm just looking for the rough edges..
+        eval(fname)(req, args);
 
     } else {
         app.io.broadcast("speak", {
@@ -43,42 +43,57 @@ app.io.route("speak", function(req) {
     }
 });
 
-app.io.route("set username", function(req) {
-    var username = req.data;
+app.io.route("set username", set_initial_username);
+
+function set_username(req, username) {
     if(clients[username] === undefined) {
         clients[username] = req.socket.id;
         clientSockets[req.socket.id] = username;
         console.log("paired username:websocket -> "+username+":"+req.socket.id);
-        req.io.respond({ OK: "username set successfully" });
+        req.io.respond({ name: "speak", OK: "username set successfully" });
     } else {
         console.log("failed to pair username:websocket, username already in use");
-        req.io.respond({ ERROR: "username in use" });
+        req.io.respond({ name: "speak", ERROR: "username in use" });
     }
-});
+}
 
+function set_initial_username(req) {
+    set_username(req, req.data);
+}
 
 //var cmds = ["join", "part", "list", "channels", "nick", "msg"];
-function cmd_join(chan) {
+function cmd_join(req, chan) {
     console.log("I was asked to join channel " + chan);
 }
 
-function cmd_part(chan) {
+function cmd_part(req, chan) {
     console.log("I was asked to part channel " + chan);
 }
 
-function cmd_list(chan) {
+function cmd_list(req, chan) {
     console.log("I was asked to list users in chan" + chan);
 }
 
-function cmd_channels() {
+function cmd_channels(req) {
     console.log("I was asked to list all channels");
 }
 
-function cmd_nick(newnick) {
+function cmd_nick(req, newnick) {
     console.log("I was asked to change nick to " + newnick);
 }
 
-function cmd_msg(target, msg) {
+function cmd_whoami(req){
+    var uname = clientSockets[req.socket.id];
+    if(uname === undefined) {
+        console.log("unable to find matching username for id " + req.socket.id);
+        req.io.respond({ message: "[error] who are you?" });
+    } else {
+        console.log("whoami => " + uname + ":" + req.socket.id);
+        req.io.emit("speak", { user: uname, message: "[server-reply] whoami => " + uname });
+    }
+}
+
+function cmd_msg(req, target, msg) {
     console.log("I was asked to tell " + target + " ''" + msg + "'");
 }
 
